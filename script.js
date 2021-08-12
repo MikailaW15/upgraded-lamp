893cb682c4cc85eaa06d6a30e8096d5bcff4aa9a
Mikaila Williams <babysis@ymail.com>
2021-08-11 21:39:28 -0400

    new variables

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 var upperCase = document.getElementById()
-var lowerCase = document.getElementById()
+var lowerCase = document.getElementById('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
 var numbers =  document.getElementById()
 var symbols = document.getElementById()
+
+var passwordLength = prompt('Must be at least 8 characters and no more than 128 characters')
+var cap = confirm()
+var lower = confirm()
+var special = confirm()
+var numbers = confirm()
+
 
 // Write password to the #password input
 function writePassword() {
